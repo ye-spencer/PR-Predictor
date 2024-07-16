61bfe0c03dea009cb16f37fa3d10416581992458
Spencer Ye <57966319+ye-spencer@users.noreply.github.com>
2024-07-16 17:10:08 +0800

    well that ends things
    project has to end since I cannot connect to things while using a VPN, and I can't connect without a VPN...

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import requests 
+from bs4 import BeautifulSoup 
+
 def main():
-    print("success")
+    athleteid = 24094886
+    athletic_url = "https://www.athletic.net/athlete/" + str(athleteid) + "/track-and-field/all"
+    test = getdata("https://www.google.com")
+    print(test.prettify())
+    
+def getdata(url):
+    r = requests.get(url) 
+    return r.text 
+
+def html_code(url): 
+  
+    htmldata = getdata(url) 
+    soup = BeautifulSoup(htmldata, 'html.parser') 
+
+    return(soup) 
 
 if __name__ == "__main__":
     main()
